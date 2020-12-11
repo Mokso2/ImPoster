14729f33de88eed42f228ceb2f7e1cfd6c20741e
Mokso <71980805+Mokso2@users.noreply.github.com>
2020-12-11 20:44:36 +0700

    Some new

--- app/src/main/java/com/example/imposterfxmain/MainActivity.java
+++ app/src/main/java/com/example/imposterfxmain/MainActivity.java
@@ -4,41 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn_History;
-    private Button btn_AddAccount;
-    private Button btn_Main;
+    float x1, x2, y1, y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addListenerOnButton();
+
     }
-    public void addListenerOnButton (){
-        btn_AddAccount=(Button)findViewById(R.id.btn_AddAccount);
-        btn_History=(Button)findViewById(R.id.btn_History);
-        btn_Main=(Button)findViewById(R.id.btn_Main);
-        btn_History.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intentHistory = new Intent(getApplicationContext(),HistoryActivity.class);
-                        startActivity(intentHistory);
-                    }
+
+    public boolean onTouchEvent(MotionEvent touchevent) {
+        switch (touchevent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchevent.getX();
+                y1 = touchevent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchevent.getX();
+                y2 = touchevent.getY();
+                if (x1 < x2) {
+                    Intent i = new Intent(MainActivity.this, AddAccountActivity.class);
+                    startActivity(i);
                 }
-        );
-        btn_AddAccount.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intentAddAccount=new Intent(getApplicationContext(),AddAccountActivity.class);
-                        startActivity(intentAddAccount);
-                    }
+        }
+        switch (touchevent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                x1 = touchevent.getX();
+                y1 = touchevent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchevent.getX();
+                y2 = touchevent.getY();
+                if (x1 > x2) {
+                    Intent i2 = new Intent(MainActivity.this, HistoryActivity.class);
+                    startActivity(i2);
                 }
-        );
-}
+
+
+        }
+        return false;
+    }
 }
