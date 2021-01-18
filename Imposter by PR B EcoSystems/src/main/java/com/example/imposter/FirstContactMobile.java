ef1491bfbb938c2474e8caf578125711b1aa7b73
chillmouse <koftun251201@inbox.ru>
2021-01-18 22:15:26 +0700

    bugs_Alpha 0.0.3

--- Imposter by PR B EcoSystems/src/main/java/com/example/imposter/FirstContactMobile.java
+++ Imposter by PR B EcoSystems/src/main/java/com/example/imposter/FirstContactMobile.java
@@ -6,26 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class FirstContactMobile extends AppCompatActivity {
-    Button btn_Join = (Button) findViewById(R.id.btn_Join);
+    private Button btn_Join;
+    private TextView myText = (TextView) findViewById(R.id.txt_descrApp);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_contact_mobile);
-
-        View.OnClickListener OclOnJoin = new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                System.out.println("Кнопку нажал - спать лёг.");
-                Intent ToMain = new Intent(FirstContactMobile.this, MainActivity.class);
-                startActivity(ToMain);
-                System.out.println("Кнопка работает");
-                System.out.println("Кнопку нажал - Максу пирожочек дал.");
-            }
-        };
-
-
+        addListenerOnButton();
     }
 
+    public void user_Login(View v){
+        myText.setText("Хи-хи-хи");
+    }
+
+    public void addListenerOnButton(){
+        btn_Join = (Button) findViewById(R.id.myButton);
+
+        btn_Join.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        System.out.println("Кнопку нажал - спать лёг.");
+                        // Intent ToMain = new Intent(FirstContactMobile.this, MainActivity.class);
+                       // startActivity(ToMain);
+                        System.out.println("Кнопка работает");
+                        System.out.println("Кнопку нажал - Максу пирожочек дал.");
+                        myText.setText("Я бомж.");
+                    }
+                }
+        );
+    }
 }
