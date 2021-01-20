533fa05a5fb83bb07a636db453d31e5024c26ff7
Mokso <71980805+Mokso2@users.noreply.github.com>
2021-01-20 20:32:01 +0700

    20.01.2 Скелет готовый и рабочий

--- app/src/main/java/com/example/imposter/Registration.java
+++ app/src/main/java/com/example/imposter/Registration.java
@@ -6,29 +6,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.imposterfxmain.R;
 
-public class FirstContactMobile extends AppCompatActivity {
-    private Button btn_toMain;
+public class Registration extends AppCompatActivity {
+private TextView txt_Entrance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_contact_mobile);
+        setContentView(R.layout.activity_registration);
         addListenerOnButton();
     }
-    public void addListenerOnButton(){
-        btn_toMain=(Button)findViewById(R.id.btn_mainsen);
-        btn_toMain.setOnClickListener(
+    public void addListenerOnButton (){
+        txt_Entrance=(TextView)findViewById(R.id.txt_Entrance);
+        txt_Entrance.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent ToMain= new Intent(FirstContactMobile.this,MainActivity.class);
-                        startActivity(ToMain);
-
+                        Intent toFirst = new Intent(Registration.this,Register.class);
+                        startActivity(toFirst);
                     }
                 }
         );
-
     }
 }
